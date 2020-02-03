e0b3ce8690db20440f067a512c76a0321715e58c
Nonny <onyejekwe.harddisk20@gmail.com>
2020-02-03 12:50:55 +0100

    Updates

---
+++ precache-manifest.17a9ea0b8bab1badd122a52908c25462.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156e30ef490657aef35fef58d4df90b0",
+    "url": "/freestyle/index.html"
+  },
+  {
+    "revision": "5e5f29d73e0cde144995",
+    "url": "/freestyle/static/css/main.2c3dddd0.chunk.css"
+  },
+  {
+    "revision": "9ed995b2a6fac8405dc0",
+    "url": "/freestyle/static/js/2.be4013d6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/freestyle/static/js/2.be4013d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e5f29d73e0cde144995",
+    "url": "/freestyle/static/js/main.2a5342b5.chunk.js"
+  },
+  {
+    "revision": "5f4f9bc8694f85bb7df4",
+    "url": "/freestyle/static/js/runtime-main.604f1913.js"
+  },
+  {
+    "revision": "179f3cb98fcced59803adb6edfc3c9a5",
+    "url": "/freestyle/static/media/Image2.179f3cb9.png"
+  },
+  {
+    "revision": "03e91f122aa5fd425abbe23c85546eb0",
+    "url": "/freestyle/static/media/Linearicons-Free.03e91f12.woff2"
+  },
+  {
+    "revision": "2781bf543c08d394dc78dec86989e3fd",
+    "url": "/freestyle/static/media/Linearicons-Free.2781bf54.svg"
+  },
+  {
+    "revision": "2f3e9f80fff7d699dd3de6904d7d1647",
+    "url": "/freestyle/static/media/Linearicons-Free.2f3e9f80.ttf"
+  },
+  {
+    "revision": "65060723fe964f85afa0a82d0bb78cf9",
+    "url": "/freestyle/static/media/Linearicons-Free.65060723.woff"
+  },
+  {
+    "revision": "b9b7f23cb61b1f503e1249b63d980448",
+    "url": "/freestyle/static/media/Linearicons-Free.b9b7f23c.eot"
+  },
+  {
+    "revision": "a037f4e0b7cb8e21042b5b78305192c8",
+    "url": "/freestyle/static/media/about.a037f4e0.png"
+  },
+  {
+    "revision": "f89a068609ef9f1400d8742d1c9faf8e",
+    "url": "/freestyle/static/media/big.f89a0686.jpg"
+  },
+  {
+    "revision": "fe37dda36632bbd1d8a451f55f826abe",
+    "url": "/freestyle/static/media/big2.fe37dda3.jpg"
+  },
+  {
+    "revision": "06c8e05aeb458266ade1085896c0332f",
+    "url": "/freestyle/static/media/icomoon.06c8e05a.svg"
+  },
+  {
+    "revision": "8275504f5b9e186b54b8d84ebdedc2b4",
+    "url": "/freestyle/static/media/icomoon.8275504f.eot"
+  },
+  {
+    "revision": "dd6d51dddf740923fe9c94859aeee2bf",
+    "url": "/freestyle/static/media/icomoon.dd6d51dd.woff"
+  },
+  {
+    "revision": "ee8d016f1bfe9220bee450ff02b59624",
+    "url": "/freestyle/static/media/icomoon.ee8d016f.ttf"
+  }
+]);
